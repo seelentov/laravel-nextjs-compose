e4fff22c6b39badb2658e42b1ef06aaaa7519309
vladisluw <komkov222111@gmail.com>
2024-10-21 17:54:12 +0500

    Add tests
    Add redis test
    Add db test on users table

---
+++ laravel/tests/Unit/DBTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DBTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_can_store_new_user()
+    {
+        $userData = [
+            "name" => "John",
+            'email' => 'john.doe@example.com',
+            'password' => bcrypt('password'),
+        ];
+
+        $newUser = User::create($userData);
+
+        $this->assertDatabaseHas('users', $userData);
+        $this->assertNotNull($newUser->id);
+    }
+
+    public function test_it_can_get_user()
+    {
+        $userData = [
+            "name" => "John",
+            'email' => 'john.doe@example.com',
+            'password' => bcrypt('password'),
+        ];
+
+        $user = User::factory()->create($userData);
+
+        $this->assertTrue(User::find($user->id) !== null);
+    }
+
+    public function test_it_can_update_user()
+    {
+        $user = User::factory()->create();
+        $updatedEmail = 'updated.email@example.com';
+
+        $user->update(['email' => $updatedEmail]);
+
+        $this->assertDatabaseHas('users', ['email' => $updatedEmail]);
+    }
+
+    public function test_it_can_delete_user()
+    {
+        $userData = [
+            "name" => "John",
+            'email' => 'john.doe@example.com',
+            'password' => bcrypt('password'),
+        ];
+
+        $user = User::factory()->create($userData);
+
+        $user->delete();
+
+        $this->assertDatabaseMissing('users', ['id' => $user->id]);
+    }
+}
---
+++ laravel/tests/Unit/RedisTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Support\Facades\Redis;
+use Tests\TestCase;
+
+class RedisTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_it_can_set_get(): void
+    {
+        Redis::set("foo", "bar");
+
+        $foo = Redis::get("foo");
+
+        $this->assertEquals("bar", $foo);
+    }
+
+    public function test_it_can_del(): void
+    {
+        Redis::set("foo", "bar");
+
+        $foo = Redis::get("foo");
+
+        Redis::del("foo");
+
+        $foo_after_del = Redis::get("foo");
+
+        $this->assertNotEquals($foo, $foo_after_del);
+    }
+
+    public function test_it_can_set_get_with_ttl(): void
+    {
+        Redis::set("foo", "bar", 'EX', 5);
+
+        $foo = Redis::get("foo");
+
+        $this->assertEquals("bar", $foo);
+
+        sleep(6);
+
+        $foo_after_ttl = Redis::get("foo");
+
+        $this->assertNull($foo_after_ttl);
+    }
+
+    public function test_it_can_store_and_retrieve_arrays(): void
+    {
+        $data = ['name' => 'John Doe', 'age' => 30];
+        Redis::set("user", json_encode($data));
+
+        $user = json_decode(Redis::get("user"), true);
+
+        $this->assertEquals($data, $user);
+    }
+
+    public function test_it_can_use_expire_to_set_ttl(): void
+    {
+        Redis::set("foo", "bar");
+
+        Redis::expire("foo", 10);
+
+        $ttl = Redis::ttl("foo");
+
+        $this->assertTrue($ttl >= 9 && $ttl <= 10);
+    }
+}
